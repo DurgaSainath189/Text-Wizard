8a2d7e914cb12a6497e17caa3f87ea181669cc37
Durga Sainath Dora <durgasainathdora@gmail.com>
2023-10-03 01:54:41 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,43 +1,63 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [mode,setMode]=useState('light');
-  const [alert,setAlert]=useState(null);
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-  const showAlert=(message,type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-      message:message,
-      type:type
-    })
-   setTimeout(() => {
-    setAlert(null);
-   }, 2000);
-  }
-  const changeMode=()=>{
-    if(mode==='dark'){
-      setMode('light');
-      document.body.style.backgroundColor='white';
-      showAlert("Switched to Light Mode","Success")
+      message: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+  const changeMode = () => {
+    if (mode === "dark") {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Switched to Light Mode", "Success");
+    } else {
+      setMode("dark");
+      document.body.style.backgroundColor = "#111111";
+      showAlert("Switched to Dark Mode", "Success");
     }
-    else{
-      setMode('dark');
-      document.body.style.backgroundColor='#111111';
-      showAlert("Switched to Dark Mode","Success")
-    }
-  }
+  };
   return (
     <>
-      <Navbar title="Text Converter" search="Search Here" mode={mode} changeMode={changeMode}/>
-      <Alert alert={alert}/>
-      <div className="container my-5">
-        <TextForm heading="Enter Your Text" showAlert={showAlert} mode={mode}/>
-        {/* <About/> */}
-      </div>
+      <BrowserRouter>
+        <Navbar
+          title="Text Wizard"
+          search="Search Here"
+          mode={mode}
+          changeMode={changeMode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-5" mode={mode}>
+          <Routes>
+            <Route path="/about" element={<About mode={mode}/>} />
+          </Routes>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter the text to analyze below"
+                  mode={mode}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,120 +1,302 @@
-import React, { useState } from "react";
+import React from "react";
 
-export default function About() {
-  const [mystyle, setMyStyle] = useState({
-    color: "black",
-    backgroundColor: "white",
-  });
-  const [btntext, setbtnText] = useState("Enable Dark Mode");
-
-  const changeMode = () => {
-    if (mystyle.backgroundColor === "#2b3035") {
-      setMyStyle({
-        color: "#2b3035",
-        backgroundColor: "white",
-      });
-      setbtnText("Enable Dark Mode");
-    } else {
-      setMyStyle({
-        color: "white",
-        backgroundColor: "#2b3035",
-      });
-      setbtnText("Enable Light Mode");
-    }
-  };
+export default function About(props) {
   return (
-    <div className="container" style={mystyle}>
-      <h1 className="my-3">About Text Converter</h1>
+    <div
+      className="container"
+      style={{
+        backgroundColor: props.mode === "light" ? "white" : "#111111",
+        color: props.mode === "dark" ? "white" : "#111111",
+      }}
+    >
+      <h1 className="my-5">About Text Wizard</h1>
       <div className="accordion accordion-flush" id="accordionFlushExample">
         <div className="accordion-item">
           <h2 className="accordion-header">
             <button
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+                fontSize:18
+              }}
               className="accordion-button collapsed"
-              style={mystyle}
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#flush-collapseOne"
               aria-expanded="false"
               aria-controls="flush-collapseOne"
             >
-              Accordion Item #1
+              <b>Number of Words and Characters</b>
             </button>
           </h2>
           <div
             id="flush-collapseOne"
             className="accordion-collapse collapse"
             data-bs-parent="#accordionFlushExample"
-            style={mystyle}
           >
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to
-              demonstrate the <code>.accordion-flush</code> className. This is
-              the first item's accordion body.
+            <div
+              className="accordion-body"
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+              }}
+            >
+              When a paragraph is entered or modified, the app analyzes the text
+              to count the number of words (separated by spaces) and the total
+              number of characters, including spaces and punctuation.
             </div>
           </div>
         </div>
         <div className="accordion-item">
           <h2 className="accordion-header">
             <button
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+                fontSize:18
+              }}
               className="accordion-button collapsed"
               type="button"
-              style={mystyle}
               data-bs-toggle="collapse"
               data-bs-target="#flush-collapseTwo"
               aria-expanded="false"
               aria-controls="flush-collapseTwo"
             >
-              Accordion Item #2
+              <b>Average Time Taken to Read</b>
             </button>
           </h2>
           <div
             id="flush-collapseTwo"
             className="accordion-collapse collapse"
             data-bs-parent="#accordionFlushExample"
-            style={mystyle}
           >
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to
-              demonstrate the <code>.accordion-flush</code> className. This is
-              the second item's accordion body. Let's imagine this being filled
-              with some actual content.
+            <div
+              className="accordion-body"
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+              }}
+            >
+              Based on typical reading speed assumptions (e.g., 200-250 words
+              per minute for average readers), the app calculates how long it
+              would take, on average, for a person to read the entire paragraph.
             </div>
           </div>
         </div>
         <div className="accordion-item">
           <h2 className="accordion-header">
             <button
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+                fontSize:18
+              }}
               className="accordion-button collapsed"
               type="button"
-              style={mystyle}
               data-bs-toggle="collapse"
               data-bs-target="#flush-collapseThree"
               aria-expanded="false"
               aria-controls="flush-collapseThree"
             >
-              Accordion Item #3
+              <b>Toggle Between Dark and Light Theme</b>
             </button>
           </h2>
           <div
             id="flush-collapseThree"
             className="accordion-collapse collapse"
-            style={mystyle}
             data-bs-parent="#accordionFlushExample"
           >
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to
-              demonstrate the <code>.accordion-flush</code> className. This is
-              the third item's accordion body. Nothing more exciting happening
-              here in terms of content, but just filling up the space to make it
-              look, at least at first glance, a bit more representative of how
-              this would look in a real-world application.
+            <div
+              className="accordion-body"
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+              }}
+            >
+              Users can change the app's visual appearance to suit their
+              preferences or adapt to different lighting conditions by toggling
+              between dark and light themes.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+                fontSize:18
+              }}
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseFour"
+              aria-expanded="false"
+              aria-controls="flush-collapseFour"
+            >
+              <b>Convert to Uppercase</b>
+            </button>
+          </h2>
+          <div
+            id="flush-collapseFour"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div
+              className="accordion-body"
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+              }}
+            >
+              When the user selects this option, all text in the paragraph is
+              transformed to uppercase, making it entirely capitalized.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+                fontSize:18
+              }}
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseFive"
+              aria-expanded="false"
+              aria-controls="flush-collapseFive"
+            >
+              <b>Convert to Lowercase</b>
+            </button>
+          </h2>
+          <div
+            id="flush-collapseFive"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div
+              className="accordion-body"
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+              }}
+            >
+              When the user selects this option, all text in the paragraph is
+              transformed to lowercase, making it entirely in small letters.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+                fontSize:18
+              }}
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseSix"
+              aria-expanded="false"
+              aria-controls="flush-collapseSix"
+            >
+              <b>Copy to Clipboard</b>
+            </button>
+          </h2>
+          <div
+            id="flush-collapseSix"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div
+              className="accordion-body"
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+              }}
+            >
+              When the user selects this option, the app uses the clipboard API
+              to copy the paragraph's content to the user's system clipboard,
+              making it easy to paste elsewhere.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+                fontSize:18
+              }}
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseSeven"
+              aria-expanded="false"
+              aria-controls="flush-collapseSeven"
+            >
+              <b>Clear Text</b> 
+            </button>
+          </h2>
+          <div
+            id="flush-collapseSeven"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div
+              className="accordion-body"
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+              }}
+            >
+              When the user selects this option, all text in the paragraph input
+              area is removed, providing a clean slate for new content.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+                fontSize:18
+              }}
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseEight"
+              aria-expanded="false"
+              aria-controls="flush-collapseEight"
+            >
+              <b>Remove Extra Spaces</b>
+            </button>
+          </h2>
+          <div
+            id="flush-collapseEight"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div
+              className="accordion-body"
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#2b3035",
+                color: props.mode === "dark" ? "white" : "#2b3035",
+              }}
+            >
+              When the user selects this option, any excess spaces between words
+              are condensed to a single space, improving the readability and
+              consistency of the text.
             </div>
           </div>
         </div>
       </div>
-      <button className="btn btn-primary my-3" onClick={changeMode}>
-        {btntext}
-      </button>
     </div>
   );
 }
